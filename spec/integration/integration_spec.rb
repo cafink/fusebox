2f92bc107f43829729661efe92d04dadf99e81dc
Carl Fink <carl@mudbugmedia.com>
2014-08-28 16:08:20 -0500

    Replace class variables in integration spec with let().

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -12,18 +12,16 @@ require 'spec_helper'
 # If you have any ideas for improving these tests, please feel free to refactor
 # or drop suggestions.
 describe Fusebox::Request do
+
+  let(:spec_domain) { SecureRandom.hex(4) + '.example.com' }
+  let(:secondary_domain) { SecureRandom.hex(4) + '.example.com' }
+  let(:forward) { SecureRandom.hex(4) + "@#{spec_domain}" }
+  let(:sleep_duration) { 30 } # How long the tests should sleep after the initial "order" to allow the data to propagate
+
   before(:all) do
     @fixtures = YAML.load(File.read(File.expand_path('~/.fusemail.yaml')))['spec']
     raise "Integration tests require 'spec' properties defined in ~/.fusemail.yaml" unless @fixtures
-
-    # These are globals shared between the tests.
-    # Is there a more appropriate way to share data between the specs?
-    @@spec_domain      ||= SecureRandom.hex(4) + '.example.com'
-    @@secondary_domain ||= SecureRandom.hex(4) + '.example.com'
-    @@forward          ||= SecureRandom.hex(4) + "@#{@@spec_domain}"
-    @@sleep_duration = 30 # How long the tests should sleep after the initial "order" to allow the data to propagate
   end
-
 
   describe "commands" do
 
@@ -35,14 +33,14 @@ describe Fusebox::Request do
       end
 
       it "should be successful when adding new group account" do
-        @response = Fusebox::Request.new.order(:account_type => @fixtures['group_account_type'], :user => "postmaster@#{@@spec_domain}", :password => SecureRandom.hex, :first_name => 'fusebox rspec sandbox', :last_name => '(delete me)')
+        @response = Fusebox::Request.new.order(:account_type => @fixtures['group_account_type'], :user => "postmaster@#{spec_domain}", :password => SecureRandom.hex, :first_name => 'fusebox rspec sandbox', :last_name => '(delete me)')
         @response.detail.should match('Order Created Succesfully')
         @response.success?.should == true
-        sleep @@sleep_duration # Let fusemail catch up.. hopefully.
+        sleep sleep_duration # Let fusemail catch up.. hopefully.
       end
 
       it "should be successful when adding group subaccounts" do
-        @response = Fusebox::Request.new.order(:account_type => 'group_subaccount', :group_parent => "postmaster@#{@@spec_domain}", :user => "user@#{@@spec_domain}", :password => SecureRandom.hex, :first_name => 'fusebox rspec sandbox', :last_name => '(delete me)')
+        @response = Fusebox::Request.new.order(:account_type => 'group_subaccount', :group_parent => "postmaster@#{spec_domain}", :user => "user@#{spec_domain}", :password => SecureRandom.hex, :first_name => 'fusebox rspec sandbox', :last_name => '(delete me)')
         @response.detail.should match('Order Created Succesfully')
         @response.success?.should == true
       end
@@ -50,7 +48,7 @@ describe Fusebox::Request do
 
     describe "modify" do
       it "should be successful" do
-        @response = Fusebox::Request.new.modify(:user => "postmaster@#{@@spec_domain}", :first_name => 'fusebox rspec sandbox modified')
+        @response = Fusebox::Request.new.modify(:user => "postmaster@#{spec_domain}", :first_name => 'fusebox rspec sandbox modified')
         @response.detail.should match('modification complete')
         @response.success?.should == true
       end
@@ -58,17 +56,17 @@ describe Fusebox::Request do
 
     describe "report" do
       it "should be return a array of hash results" do
-        @response = Fusebox::Request.new.report('user' => "postmaster@#{@@spec_domain}", 'group_subaccount' => 'yes')
+        @response = Fusebox::Request.new.report('user' => "postmaster@#{spec_domain}", 'group_subaccount' => 'yes')
         @response.records.should be_instance_of(Array)
         @response.records.first.should be_instance_of(Hash)
-        @response.records.map { |r| r[:username] }.should include("postmaster@#{@@spec_domain}")
+        @response.records.map { |r| r[:username] }.should include("postmaster@#{spec_domain}")
         @response.success?.should == true
       end
     end
 
     describe "adddomain" do
       it "should be successful" do
-        @response = Fusebox::Request.new.adddomain(:domain => @@secondary_domain, :user => "postmaster@#{@@spec_domain}")
+        @response = Fusebox::Request.new.adddomain(:domain => secondary_domain, :user => "postmaster@#{spec_domain}")
         @response.detail.should match('Domain was added')
         @response.success?.should == true
       end
@@ -76,7 +74,7 @@ describe Fusebox::Request do
 
     describe "checkdomain" do
       it "should not be successful for existing domains" do
-        @response = Fusebox::Request.new.checkdomain(:domain => @@secondary_domain)
+        @response = Fusebox::Request.new.checkdomain(:domain => secondary_domain)
         @response.detail.should match('already exists')
         @response.success?.should == false
       end
@@ -90,7 +88,7 @@ describe Fusebox::Request do
 
     describe "removedomain" do
       it "should be successful" do
-        @response = Fusebox::Request.new.removedomain(:domain => @@secondary_domain)
+        @response = Fusebox::Request.new.removedomain(:domain => secondary_domain)
         @response.detail.should match('Domain was removed')
         @response.success?.should == true
       end
@@ -99,7 +97,7 @@ describe Fusebox::Request do
 
     describe "addforward" do
       it "should be successful" do
-        @response = Fusebox::Request.new.addforward(:forward_what => @@forward, :forward_to => "postmaster@#{@@spec_domain}", :user => "postmaster@#{@@spec_domain}")
+        @response = Fusebox::Request.new.addforward(:forward_what => forward, :forward_to => "postmaster@#{spec_domain}", :user => "postmaster@#{spec_domain}")
         @response.detail.should match('Your forwarder has been successfully submitted')
         @response.success?.should == true
       end
@@ -107,15 +105,15 @@ describe Fusebox::Request do
 
     describe "getforward" do
       it "should be successful for existing forwards" do
-        @response = Fusebox::Request.new.getforward(:forward_what => @@forward, :user => "postmaster@#{@@spec_domain}")
-        @response.detail.should match("postmaster@#{@@spec_domain}")
+        @response = Fusebox::Request.new.getforward(:forward_what => forward, :user => "postmaster@#{spec_domain}")
+        @response.detail.should match("postmaster@#{spec_domain}")
         @response.success?.should == true
       end
     end
 
     describe "removeforward" do
       it "should be successful" do
-        @response = Fusebox::Request.new.removeforward(:forward_what => @@forward, :forward_to => "postmaster@#{@@spec_domain}", :user => "postmaster@#{@@spec_domain}")
+        @response = Fusebox::Request.new.removeforward(:forward_what => forward, :forward_to => "postmaster@#{spec_domain}", :user => "postmaster@#{spec_domain}")
         @response.detail.should match('Your forwarder has been successfully removed')
         @response.success?.should == true
       end
@@ -124,14 +122,14 @@ describe Fusebox::Request do
 
     describe "checkalias" do
       it "should not be successful for existing aliases" do
-        @response = Fusebox::Request.new.checkalias(:alias => "postmaster@#{@@spec_domain}")
+        @response = Fusebox::Request.new.checkalias(:alias => "postmaster@#{spec_domain}")
         @response.should be_instance_of(Fusebox::Response)
         @response.detail.should match('already taken')
         @response.success?.should == false
       end
 
       it "should be successful for new aliases" do
-        @response = Fusebox::Request.new.checkalias(:alias => SecureRandom.hex + "@#{@@spec_domain}")
+        @response = Fusebox::Request.new.checkalias(:alias => SecureRandom.hex + "@#{spec_domain}")
         @response.detail.should match('Alias is Available')
         @response.success?.should == true
       end
@@ -139,7 +137,7 @@ describe Fusebox::Request do
 
     describe "suspend" do
       it "should be successful when suspending existing accounts" do
-        @response = Fusebox::Request.new.suspend(:user => "postmaster@#{@@spec_domain}")
+        @response = Fusebox::Request.new.suspend(:user => "postmaster@#{spec_domain}")
         @response.detail.should match('Account Succesfully Suspended')
         @response.success?.should == true
       end
@@ -147,7 +145,7 @@ describe Fusebox::Request do
 
     describe "enable" do
       it "should be successful when enabling suspended accounts" do
-        @response = Fusebox::Request.new.enable(:user => "postmaster@#{@@spec_domain}")
+        @response = Fusebox::Request.new.enable(:user => "postmaster@#{spec_domain}")
         @response.detail.should match('Account Successfully Enabled')
         @response.success?.should == true
       end
@@ -155,13 +153,13 @@ describe Fusebox::Request do
 
     describe "terminate" do
       it "should not be successful when purging non-existing accounts" do
-        @response = Fusebox::Request.new.terminate(:user => SecureRandom.hex + "@#{@@spec_domain}", :purge => true)
+        @response = Fusebox::Request.new.terminate(:user => SecureRandom.hex + "@#{spec_domain}", :purge => true)
         @response.detail.should match('Terminate account failed Could not find user')
         @response.success?.should == false
       end
 
       it "should be successful purge existing accounts" do
-        @response = Fusebox::Request.new.terminate(:user => "postmaster@#{@@spec_domain}", :purge => true)
+        @response = Fusebox::Request.new.terminate(:user => "postmaster@#{spec_domain}", :purge => true)
         @response.detail.should match('Account Successfully Terminated')
         @response.success?.should == true
       end
